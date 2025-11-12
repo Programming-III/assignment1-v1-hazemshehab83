e692453a56b6fe51141214e4636b563a067cf060
hazemshehab83 <hazemshehab675@gmail.com>
2025-11-12 15:11:29 +0200

    Define enclosure class with animal management methods
    Added enclosure class with methods to manage animals.

--- Enclosure.h
+++ Enclosure.h
@@ -2,14 +2,23 @@
 #define ENCLOSURE_H
 
 #include "Animal.h"
+#include <string>
+using namespace std;
+class enclose {
 
-//define enclosure class here 
+private:
+Animals* animalslist;
+int capasity;
+int currentCount;
 
+public;
+enclose(int capacity, animalslist[], int currentCount);
 
+~enclose() {};
 
+void addAnimal(Animal animal);
+cout << "Animal added to the enclosure." << Animalslist[currentCount++] << endl;
+void displayAnimals();
+cout << "Animals in the enclosure:" << Animalslist[i] << endl;
 
-
-
-
-
-#endif
+}
